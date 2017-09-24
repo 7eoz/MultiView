90cfc0414688cd68a25af308c6be0505021e657d
Sandrini <thesandrini@gmail.com>
2017-09-24 14:14:27 -0300

    Initial commit

---
+++ app/src/main/java/com/studies/sandrini/multiview/MainActivity.java
@@ -0,0 +1,29 @@
+package com.studies.sandrini.multiview;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Tela2.class);
+                Bundle params = new Bundle();
+                params.putString("msg", "Olá");
+                intent.putExtras(params);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/studies/sandrini/multiview/Tela2.java
@@ -0,0 +1,34 @@
+package com.studies.sandrini.multiview;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class Tela2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela2);
+
+        Button button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Intent intent = new Intent(Tela2.this, MainActivity.class);
+                Intent intent = getIntent();
+                if(intent != null) {
+                    Bundle params = intent.getExtras();
+                    if(params != null) {
+                        String msg = params.getString("msg");
+                        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
+                    }
+                }
+                startActivity(intent);
+            }
+        });
+    }
+}
